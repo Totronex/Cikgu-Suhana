e0570b7dc645408f4f13e34a24dcc32b3c5c3fcd
Muhammad Iqbal <35318976+Totronex@users.noreply.github.com>
2018-08-29 15:42:36 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Maklumat Pangkalan Data
+$databaseHost = 'localhost'; //Nama Server Local
+$databaseName = 'simpan'; //Nama Pangkalan Data
+$databaseUsername = 'root'; //Nama Pengguna Pangakalan Data
+$databasePassword = '123456'; //Tiada Kata Laluan untuk Pangkalan Data
+
+$mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+?>
